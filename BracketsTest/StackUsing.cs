ba749ebb6cd20de9242c21d6c5a2b3e40a32f3b9
D O <daria.osipova@nure.ua>
2018-10-21 17:58:46 +0300

    new tests

--- BracketsTest/StackUsing.cs
+++ BracketsTest/StackUsing.cs
@@ -8,16 +8,28 @@ namespace BracketsTest
     public class BracketTest
     {
         [TestMethod]
-        public void TestStackUse1()
+        public void TestStackUseManyBrackets()
         {
             // expected | what needs test
             Assert.AreEqual(true, StackUsing.CheckBKT("([{}])()[]<[()]>"));
         }
 
         [TestMethod]
-        public void TestStackUse2()
+        public void TestStackUseWordInsideBrackets()
         {
             Assert.AreEqual(true, StackUsing.CheckBKT("(да-да, я )"));
+        }
+
+        [TestMethod]
+        public void TestStackWordsInsideManyBrackets()
+        {
+            Assert.AreEqual(true, StackUsing.CheckBKT("{array[5] arr = new array(10)}"));
+        }
+
+        [TestMethod]
+        public void TestFailWrongBrackets()
+        {
+            Assert.AreEqual(false, StackUsing.CheckBKT("))()"));
         }
     }
 }
